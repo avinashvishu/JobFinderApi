707acb66a758a674dd0aa2e36f9795350eebc376
V Avinash <loginavi1@gmail.com>
2024-01-24 01:56:53 +0530

    added database config and initialzation

--- server.js
+++ server.js
@@ -1,5 +1,29 @@
 const express = require("express");
+const mongoose = require("mongoose");
 require("dotenv").config();
 const app = express();
 
-const Port = PORT || 3000;
+const Port = process.env.PORT || 3000;
+
+//DataBase Connection
+mongoose
+  .connect(process.env.MongoURI)
+  .then(() => {
+    console.log("Succesfully connected to the DataBase");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+// Health check Api
+app.get("/health", (req, res) => {
+  res.json({
+    message: "Server is Running Fine",
+    status: "Active",
+    time: new Date(),
+  });
+});
+
+app.listen(Port, () => {
+  console.log("server in active on port ", Port);
+});
